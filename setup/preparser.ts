c87511588368aee207ab961ebaadaf573fac4d33
Floy <git@floy.soy>
2023-05-29 00:51:40 +0200

    add quoted include because vue is a mystery

--- setup/preparser.ts
+++ setup/preparser.ts
@@ -1,6 +1,14 @@
 import { definePreparserSetup } from '@slidev/types';
 import fs from 'node:fs';
 
+function readLinesFromFile(path: string): string[] {
+    return fs.readFileSync(path)
+      .toString()
+      .split('\n')
+      // remove trailing newline
+      .slice(0, -1);
+}
+
 export default definePreparserSetup(() => {
   return {
       transformRawLines(lines) {
@@ -8,10 +16,17 @@ export default definePreparserSetup(() => {
         while (i < lines.length) {
           const match = lines[i].match(/^@src: (.*)$/);
           if (match) {
-            const fileLines = fs.readFileSync(match[1])
-              .toString()
-              .split('\n');
+            const fileLines = readLinesFromFile(match[1]);
             lines.splice(i, 1, ...fileLines);
+          }
+
+          const match2 = lines[i].match(/^@src-quot: (.*)$/);
+          if (match2) {
+            const fileLines = readLinesFromFile(match2[1]);
+            const quotedLines = fileLines.map(
+              line => line.replace(/\"/g, '&quot;')
+            );
+            lines.splice(i, 1, ...quotedLines);
           }
           i++;
         }
